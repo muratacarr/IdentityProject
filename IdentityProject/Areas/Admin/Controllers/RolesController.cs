fa8fd0668a6f50c6f78dacb6e96c3d0e9042d936
muratacarr <acarrmuratt@gmail.com>
2023-10-01 23:18:23 +0300

    roles başlangıç

---
+++ IdentityProject/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
